f8e4e31fe3e21decac2111385ff3eec0b1ba9a0f
ShevchukOleh <shew4ukoleg12@gmail.com>
2023-03-09 20:46:53 +0200

    second commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const divEl = document.querySelector('.gallery');
+
+const cardsMarkup = createGalleryItems(galleryItems);
+
+divEl.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+            </a>
+        `
+    }).join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { caption: true, captionDelay: 250 });
